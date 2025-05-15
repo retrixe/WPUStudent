1573c93a2239d6961f18e9f17920ba0b98490702
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2025-05-16 03:56:21 +0530

    Use Compose Navigation in nested Main graph

--- app/src/main/java/xyz/retrixe/wpustudent/screens/MainContainer.kt
+++ app/src/main/java/xyz/retrixe/wpustudent/screens/MainContainer.kt
@@ -1,9 +1,16 @@
 package xyz.retrixe.wpustudent.screens
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffoldDefaults
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteType
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
@@ -11,11 +18,17 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavGraph
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.get
 import kotlinx.serialization.Serializable
+import xyz.retrixe.wpustudent.R
 import xyz.retrixe.wpustudent.models.SessionViewModel
 import xyz.retrixe.wpustudent.screens.loading.LoadingScreen
 import xyz.retrixe.wpustudent.screens.login.LoginScreen
@@ -29,7 +42,15 @@ import xyz.retrixe.wpustudent.state.LocalSnackbarHostState
 object Screens {
     @Serializable object Loading
     @Serializable object Login
-    @Serializable object Main
+    @Serializable object Main {
+        @Serializable object Home
+        @Serializable object Attendance
+
+        enum class Destinations(val label: String, @DrawableRes val icon: Int, val route: Any) {
+            HOME("Home", R.drawable.baseline_home_24, Home),
+            ATTENDANCE("Attendance", R.drawable.baseline_co_present_24, Attendance),
+        }
+    }
 }
 
 @Composable
@@ -43,6 +64,9 @@ fun MainContainer(
     val loading by sessionViewModel.loading.collectAsState()
     val accessToken by sessionViewModel.accessToken.collectAsState()
     val studentBasicInfo by sessionViewModel.studentBasicInfo.collectAsState(null)
+
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
 
     LaunchedEffect(loading, accessToken) {
         // Default is Loading, we never return
@@ -67,10 +91,53 @@ fun MainContainer(
         CompositionLocalProvider(
             LocalSnackbarHostState provides snackbarHostState,
         ) {
-            NavHost(navController = navController, startDestination = Screens.Loading) {
-                composable<Screens.Loading> { LoadingScreen(innerPadding) }
-                composable<Screens.Login> { LoginScreen(innerPadding, sessionViewModel) }
-                composable<Screens.Main> { MainScreen(innerPadding, "$accessToken $studentBasicInfo") }
+            // https://developer.android.com/develop/ui/compose/layouts/adaptive/build-adaptive-navigation
+            val isNavigationBarVisible =
+                currentDestination?.route?.startsWith(Screens.Main::class.qualifiedName!!) == true
+            NavigationSuiteScaffold(
+                layoutType =
+                    if (isNavigationBarVisible)
+                        NavigationSuiteScaffoldDefaults
+                            .calculateFromAdaptiveInfo(currentWindowAdaptiveInfo())
+                    else NavigationSuiteType.None,
+                navigationSuiteItems = {
+                    val mainGraph = navController.graph[Screens.Main] as NavGraph
+                    Screens.Main.Destinations.entries.forEach {
+                        item(
+                            icon = { Icon(painterResource(it.icon), it.label) },
+                            label = { Text(it.label) },
+                            selected = mainGraph[it.route] == currentDestination,
+                            onClick = {
+                                navController.navigate(it.route) {
+                                    // Pop up to the start destination of the graph to
+                                    // avoid building up a large stack of destinations
+                                    // on the back stack as users select items
+                                    popUpTo(mainGraph.startDestinationId) {
+                                        saveState = true
+                                    }
+                                    // Avoid multiple copies of the same destination when
+                                    // reselecting the same item
+                                    launchSingleTop = true
+                                    // Restore state when reselecting a previously selected item
+                                    restoreState = true
+                                }
+                            }
+                        )
+                    }
+                }
+            ) {
+                NavHost(navController = navController, startDestination = Screens.Loading) {
+                    composable<Screens.Loading> { LoadingScreen(innerPadding) }
+                    composable<Screens.Login> { LoginScreen(innerPadding, sessionViewModel) }
+                    navigation<Screens.Main>(startDestination = Screens.Main.Home) {
+                        composable<Screens.Main.Home> {
+                            MainScreen(innerPadding, "$accessToken $studentBasicInfo")
+                        }
+                        composable<Screens.Main.Attendance> {
+                            MainScreen(innerPadding, "Attendance $accessToken $studentBasicInfo")
+                        }
+                    }
+                }
             }
         }
     }
--- app/src/main/java/xyz/retrixe/wpustudent/screens/main/MainScreen.kt
+++ app/src/main/java/xyz/retrixe/wpustudent/screens/main/MainScreen.kt
@@ -1,46 +1,16 @@
 package xyz.retrixe.wpustudent.screens.main
 
-import androidx.annotation.DrawableRes
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
-import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import xyz.retrixe.wpustudent.R
-
-enum class AppDestinations(val label: String, @DrawableRes val icon: Int) {
-    HOME("Home", R.drawable.baseline_home_24),
-    ATTENDANCE("Attendance", R.drawable.baseline_co_present_24),
-}
 
 @Composable
 fun MainScreen(paddingValues: PaddingValues, accessToken: String) {
-    var currentDestination by rememberSaveable { mutableStateOf(AppDestinations.HOME) }
-
-    // https://developer.android.com/develop/ui/compose/layouts/adaptive/build-adaptive-navigation
-    NavigationSuiteScaffold(
-        navigationSuiteItems = {
-            AppDestinations.entries.forEach {
-                item(
-                    icon = { Icon(painterResource(it.icon), it.label) },
-                    label = { Text(it.label) },
-                    selected = it == currentDestination,
-                    onClick = { currentDestination = it }
-                )
-            }
-        }
-    ) {
-        // FIXME: Destination content.
-        Box(Modifier.padding(paddingValues)) {
-            Text("FIXME $accessToken")
-        }
+    // FIXME: Destination content.
+    Box(Modifier.padding(paddingValues)) {
+        Text("FIXME $accessToken")
     }
 }
