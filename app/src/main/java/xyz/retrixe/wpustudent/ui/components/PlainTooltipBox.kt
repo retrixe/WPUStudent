5eca33d33fa9aa45fb233f1fb714c29cc79b9d5d
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2025-09-21 23:54:22 +0530

    Replace rememberPlainTooltipPositionProvider

--- app/src/main/java/xyz/retrixe/wpustudent/ui/components/PlainTooltipBox.kt
+++ app/src/main/java/xyz/retrixe/wpustudent/ui/components/PlainTooltipBox.kt
@@ -3,6 +3,7 @@ package xyz.retrixe.wpustudent.ui.components
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.PlainTooltip
 import androidx.compose.material3.Text
+import androidx.compose.material3.TooltipAnchorPosition
 import androidx.compose.material3.TooltipBox
 import androidx.compose.material3.TooltipDefaults
 import androidx.compose.material3.rememberTooltipState
@@ -12,7 +13,7 @@ import androidx.compose.runtime.Composable
 @Composable
 fun PlainTooltipBox(description: String, content: @Composable () -> Unit) =
     TooltipBox(
-        positionProvider = TooltipDefaults.rememberPlainTooltipPositionProvider(),
+        positionProvider = TooltipDefaults.rememberTooltipPositionProvider(TooltipAnchorPosition.Above),
         tooltip = { PlainTooltip { Text(description) } },
         state = rememberTooltipState(),
         content = content,
