2154240b1ea5db0d7e45067e6a9f020140af9368
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2025-09-23 11:04:41 +0530

    Use MaterialExpressiveTheme

--- app/src/main/java/xyz/retrixe/wpustudent/ui/theme/Theme.kt
+++ app/src/main/java/xyz/retrixe/wpustudent/ui/theme/Theme.kt
@@ -2,7 +2,8 @@ package xyz.retrixe.wpustudent.ui.theme
 
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.MaterialExpressiveTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -32,6 +33,7 @@ private val LightColorScheme = lightColorScheme(
     */
 )
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun WPUStudentTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
@@ -49,7 +51,7 @@ fun WPUStudentTheme(
         else -> LightColorScheme
     }
 
-    MaterialTheme(
+    MaterialExpressiveTheme(
         colorScheme = colorScheme,
         typography = Typography,
         content = content
