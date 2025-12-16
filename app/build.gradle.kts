a4765d01c0854dd894da629519c4d81770671cb4
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2025-12-16 23:47:42 +0530

    1.2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "xyz.retrixe.wpustudent"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.2.2"
+        versionCode = 9
+        versionName = "1.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
